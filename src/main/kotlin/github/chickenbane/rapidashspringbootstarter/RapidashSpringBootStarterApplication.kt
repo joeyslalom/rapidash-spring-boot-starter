b9bc2c6f02063bd59b3b05f9f1f60b6be5137969
Joey T <thechickenbane@gmail.com>
2019-02-09 13:55:49 -0800

    First commit stuff
    Create GitHub repo and basic gradle + kotlin start.spring.io project.

---
+++ src/main/kotlin/github/chickenbane/rapidashspringbootstarter/RapidashSpringBootStarterApplication.kt
@@ -0,0 +1,12 @@
+package github.chickenbane.rapidashspringbootstarter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RapidashSpringBootStarterApplication
+
+fun main(args: Array<String>) {
+	runApplication<RapidashSpringBootStarterApplication>(*args)
+}
+
